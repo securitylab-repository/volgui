c724ea556d8c49f5f68812ff37c02cc50830d202
SecurityLab <boussad.aitsalem@gmail.com>
2019-11-06 16:36:12 +0100

    volgui can detect the profile of the dump so it is not able yet to used it in other volatility commands like pslist pstree,...

--- volgui_mainwin.py
+++ volgui_mainwin.py
@@ -1,84 +1,100 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QMainWindow, QWidget,QFileDialog, QMessageBox, QMdiSubWindow, QVBoxLayout 
+from PyQt5.QtWidgets import QMainWindow, QWidget, QFileDialog, QMessageBox, QMdiSubWindow, QVBoxLayout
+from PyQt5.QtCore import pyqtSlot, QMetaObject, Qt, QThread
+
 from ui_volgui_mainwin import Ui_MainWindow
-
-from PyQt5.QtCore import  pyqtSlot , QMetaObject, Qt , QThread  
-
-
-from volgui_pstree_mdiwindow import VolGuiPstreeMdiWindow
-from volgui_pslist_mdiwindow import VolGuiPslistMdiWindow
 from volgui_console_subwindow import VolGuiConsoleMdiWindow
+from volgui_pslist_mdiwindow import VolGuiPslistMdiWindow
 from volgui_pstree_graph import *
+from volgui_pstree_mdiwindow import VolGuiPstreeMdiWindow
+from volgui_cmd import *;
+
+import subprocess
+from volgui_profile import *
 
 class VolGuiMainWindow(QMainWindow, Ui_MainWindow):
 
+    """VolGuiMainWindow inherits from Ui_MainWindow
 
-    def __init__(self,parent=None):
+    :param   Ui_MainWindow : generated with QtDesigner
 
-        super(VolGuiMainWindow,self).__init__(parent)
+    """
+
+    def __init__(self, parent=None):
+
+        super(VolGuiMainWindow, self).__init__(parent)
         self.setupUi(self)
-        QMetaObject.connectSlotsByName(self)  
+        QMetaObject.connectSlotsByName(self)
 
-        self.dumpname = None # nom du fichier dump a traiter
-        
-        self.console = None 
+        self.dumpname = None  # the name of a dump file to analyse
+
+        self.console = None
         self.pslistWindow = None
         self.pstreeWindow = None
-       
-        self.close_cpt = 0
-        #self.action_Ouvrir.triggered.connect(self.on_action_Ouvrir_triggered)
 
-##################################################################################################
-# Gestion du menu Fichier --> Ouvrir
-#################################################################################################
+        self.close_cpt = 0
+        # self.action_Ouvrir.triggered.connect(self.on_action_Ouvrir_triggered)
+
+    ##################################################################################################
+    # Gestion du menu Fichier --> Ouvrir
+    #################################################################################################
     @pyqtSlot()
     def on_action_Ouvrir_triggered(self):
 
         """
-            reponse a un click sur l action Ouvrir du menu Fichier
+            Click on  Fichier --> Ouvrir event handler
             
         """
-        
+
         if self.dumpname is None:
 
-            
-          #  self.dumpname, _ = QFileDialog.getOpenFileName(self,"Ouvrir un fichier dump","/home/debian") 
-          dumpname = None
-          dlg = QFileDialog()
-          dlg.setFileMode(QFileDialog.ExistingFile)
-          if dlg.exec_():
-              dumpname = dlg.selectedFiles()[0]
-          
-          if dumpname is not None:
+            #  self.dumpname, _ = QFileDialog.getOpenFileName(self,"Ouvrir un fichier dump","/home/debian")
+            dumpname = None
+            dlg = QFileDialog()
+            dlg.setFileMode(QFileDialog.ExistingFile)
+            if dlg.exec_():
+                dumpname = dlg.selectedFiles()[0]
 
-              self.dumpname = dumpname
-              self.action_Pslist.setEnabled(True)
-              self.action_Console.setEnabled(True)
-              self.action_Pstree_Graph.setEnabled(True)
+            if dumpname is not None:
+
+                self.dumpname = dumpname
+
+                # build image profile
+                command = ['vol.py', '-f', self.dumpname, 'imageinfo', '--output=json']
+                p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                cmd_result, err = p.communicate()
+
+                if cmd_result:
+                    self.profile = Profile()
+                    self.profile.build_profile(cmd_result.decode('utf-8'))
+
+                self.action_Pslist.setEnabled(True)
+                self.action_Console.setEnabled(True)
+                self.action_Pstree_Graph.setEnabled(True)
 
 
-#######################################################################################################
-# Gestion de la fenetre PSLIST    
-######################################################################################################
+    #######################################################################################################
+    # Gestion de la fenetre PSLIST
+    ######################################################################################################
 
     @pyqtSlot()
     def on_action_Pslist_triggered(self):
 
         if self.action_Pslist.isChecked():
-            
+
             self.pslist()
 
         else:
-            
+
             self.pslistWindow.hide()
             self.mdi.tileSubWindows()
 
     def pslist(self):
-        
+
         if not self.pslistWindow:
-            
-            self.pslistWindow = VolGuiPslistMdiWindow(self.dumpname,self)
+
+            self.pslistWindow = VolGuiPslistMdiWindow(self.dumpname, self)
             self.pslistWindow.mdiWindowClosed.connect(self.pslistWindowClosed)
             self.pslistWindow.mdiWindowReady.connect(self.pslistWindowReady)
             self.pslistWindow.newMdiWindowAdded.connect(self.on_newMdiWindow)
@@ -87,94 +103,82 @@ class VolGuiMainWindow(QMainWindow, Ui_MainWindow):
 
             self.updatePslistWindow()
 
-        
-            
-    
     def updatePslistWindow(self):
-        
+
         self.pslistWindow.show()
         self.mdi.tileSubWindows()
-        
-    
+
     def pslistWindowClosed(self):
-        
+
         self.action_Pslist.setChecked(False)
         self.pslistWindow = None
         self.mdi.tileSubWindows()
 
-      
-    def pslistWindowReady(self,winame): 
-       
+    def pslistWindowReady(self, winame):
+
         self.mdi.addSubWindow(self.pslistWindow)
         self.pslistWindow.show()
         self.mdi.tileSubWindows()
-        
-       
 
-#######################################################################################################################
-# Gestion de la fermeture de l'application
-######################################################################################################################
+    #######################################################################################################################
+    # Gestion de la fermeture de l'application
+    ######################################################################################################################
 
-    def closeEvent(self,event):
+    def closeEvent(self, event):
 
-    #    if self.close_cpt == 0:
-            messageConfirmation = "Are you closing application ?"
-            reponse = QMessageBox.question(self,"Confirmation",messageConfirmation, QMessageBox.Yes, QMessageBox.No)
-     #       self.close_cpt = self.close_cpt + 1
-            
-            if reponse == QMessageBox.Yes :
-                event.accept()
-            else:
-                event.ignore()
-      #  else:
-       #     event.ignore()
+        #    if self.close_cpt == 0:
+        messageConfirmation = "Are you closing application ?"
+        reponse = QMessageBox.question(self, "Confirmation", messageConfirmation, QMessageBox.Yes, QMessageBox.No)
+        #       self.close_cpt = self.close_cpt + 1
 
-    
+        if reponse == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    #  else:
+    #     event.ignore()
+
     @pyqtSlot()
     def on_action_Quitter_2_triggered(self):
         self.close()
 
-##########################################################################################################################
-# Gestion de la console
-#########################################################################################################################
+    ##########################################################################################################################
+    # Gestion de la console
+    #########################################################################################################################
 
     @pyqtSlot()
     def on_action_Console_triggered(self):
 
         if not self.console:
-        
-            self.console = VolGuiConsoleMdiWindow(self.dumpname,self)
+
+            self.console = VolGuiConsoleMdiWindow(self.dumpname, self)
             self.console.mdiWindowClosed.connect(self.consoleWindowClosed)
             self.console.mdiWindowReady.connect(self.consoleWindowReady)
-            self.console.prepareWindow() 
+            self.console.prepareWindow()
 
         else:
             self.updateConsole()
 
     def updateConsole(self):
-        
+
         self.console.show()
         self.mdi.tileSubWindows()
-
 
     def consoleWindowReady(self, winname):
 
-        
         self.mdi.addSubWindow(self.console)
         self.console.show()
         self.mdi.tileSubWindows()
-        
-    
+
     def consoleWindowClosed(self):
-        
+
         self.console = None
         self.mdi.tileSubWindows()
 
-
-
-##########################################################################################################################
-# Gestion du graphe pstree
-#########################################################################################################################
+    ##########################################################################################################################
+    # Gestion du graphe pstree
+    #########################################################################################################################
 
     @pyqtSlot()
     def on_action_Pstree_Graph_triggered(self):
@@ -182,66 +186,54 @@ class VolGuiMainWindow(QMainWindow, Ui_MainWindow):
         if self.action_Pstree_Graph.isChecked():
             self.pstree()
         else:
-            
 
             self.pstreeWindow.hide()
             self.mdi.tileSubWindows()
 
-
-    
     def pstree(self):
-        
+
         if not self.pstreeWindow:
-            
-            self.pstreeWindow = VolGuiPstreeMdiWindow(self.dumpname,self)
+
+            self.pstreeWindow = VolGuiPstreeMdiWindow(self.dumpname, self)
             self.pstreeWindow.mdiWindowClosed.connect(self.pstreeWindowClosed)
             self.pstreeWindow.mdiWindowReady.connect(self.pstreeWindowReady)
             self.pstreeWindow.newMdiWindowAdded.connect(self.on_newMdiWindow)
-            self.pstreeWindow.prepareWindow() 
+            self.pstreeWindow.prepareWindow()
 
         else:
             self.updatePstreeWindow()
- 
-    
+
     def updatePstreeWindow(self):
-        
+
         self.pstreeWindow.show()
         self.mdi.tileSubWindows()
 
+    def pstreeWindowReady(self, winname):
 
-    def pstreeWindowReady(self,winname):
-        
         self.mdi.addSubWindow(self.pstreeWindow)
         self.pstreeWindow.show()
         self.mdi.tileSubWindows()
-        
-    
+
     def pstreeWindowClosed(self):
-        
 
         self.action_Pstree_Graph.setChecked(False)
         self.pstreeWindow = None
         self.mdi.tileSubWindows()
 
+    ##########################################################################################################################
+    # Gestion des Logs
+    #########################################################################################################################
 
-##########################################################################################################################
-# Gestion des Logs
-#########################################################################################################################
+    def addLog(self, winname, message):
 
-    def addLog(self,winname,message):
+        print("Window Name: {0}     Message: {1}".format(winname, message))
 
-        print("Window Name: {0}     Message: {1}".format(winname,message))
+    ############################################################################################################################
+    # For All
+    ############################################################################################################################
 
+    def on_newMdiWindow(self, window):
 
-
-############################################################################################################################
-# For All
-############################################################################################################################
-
-
-    def on_newMdiWindow(self,window):
-        
         self.mdi.addSubWindow(window)
         window.show()
         self.mdi.tileSubWindows()
-
---
+++ volgui_profile.py
@@ -0,0 +1,54 @@
+# -*- coding: latin_1 -*-
+import json
+
+
+class Profile:
+    """
+    This class manage all stuffs related to volatility profile
+
+    """
+
+    def __init__(self):
+
+        self.profile = {} # content of the profile, ex: {'KPCR for CPU 0': 2190789632, 'Suggested Profile(s)': 'Win7SP1x86_23418, Win7SP0x86, Win7SP1x86_24000, Win7SP1x86', 'Image Type (Service Pack)': 0, 'Image local date and time': '2016
+
+    def build_profile(self, imageinfo_result):
+        """
+        Build a profile of the current image dump
+
+        :param imageinfo_result: the result of the imageinfo command
+        :return: None
+        """
+
+        elt_level0 = json.loads(imageinfo_result)
+        profile_fields = elt_level0['columns'][0:]
+        profile_fields_values = elt_level0['rows'][0]
+        index = 0
+        for field in profile_fields:
+            self.profile[field] = profile_fields_values[index]
+            index += 1
+
+        suggested_profile = self.profile['Suggested Profile(s)']
+        suggested_profile_list = suggested_profile.split(",")
+        self.profile['choosen_profile'] = suggested_profile_list[0]
+
+    def __repr__(self):
+        """
+        called when we call repr buildin method or enter the object name on the python cmdline
+
+        :return: the object string representation
+        """
+
+        return "Profile list :".format(self.profiles)
+
+    def __str__(self):
+        """
+        called when we print the object with print function or convert the object to string with str function
+
+        :return: the string representation of the object
+        """
+
+        return "Profile list : {}".format(self.profiles)
+
+    # def __del__(self):
+    # put here the message to log when one instance of this object is deleted
